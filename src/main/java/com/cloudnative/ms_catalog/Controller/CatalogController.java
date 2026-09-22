2676dfeffd62732095a45887d48107cf04ff751e
dainz <mbenjaminarancibia@gmail.com>
2026-09-22 00:08:56 -0300

    slop

--- src/main/java/com/cloudnative/ms_catalog/Controller/CatalogController.java
+++ src/main/java/com/cloudnative/ms_catalog/Controller/CatalogController.java
@@ -1,4 +1,4 @@
-package com.cloudnative.ms_catalog.controller;
+package com.cloudnative.ms_catalog.Controller;
 
 import com.cloudnative.ms_catalog.dto.*;
 import com.cloudnative.ms_catalog.service.CatalogService;
@@ -16,22 +16,28 @@ import java.util.List;
 public class CatalogController {
     private final CatalogService service;
 
-    public CatalogController(CatalogService service) { this.service = service; }
+    public CatalogController(CatalogService service) {
+        this.service = service;
+    }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('Admin', 'Operador')")
-    public List<ProductResponse> findAll() { return service.findAll(); }
+    @PreAuthorize("hasAnyRole('Admin', 'User')")
+    public List<ProductResponse> findAll() {
+        return service.findAll();
+    }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('Admin', 'Operador')")
-    public ProductResponse findById(@PathVariable Long id) { return service.findById(id); }
+    @PreAuthorize("hasAnyRole('Admin', 'User')")
+    public ProductResponse findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
 
     @PostMapping
     @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request) {
         ProductResponse product = service.create(request);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{id}").buildAndExpand(product.id()).toUri()).body(product);
+                .path("/{id}").buildAndExpand(product.id()).toUri()).body(product);
     }
 
     @PutMapping("/{id}")
@@ -48,7 +54,7 @@ public class CatalogController {
     }
 
     @PatchMapping("/{id}/stock")
-    @PreAuthorize("hasAnyRole('Admin', 'Operador')")
+    @PreAuthorize("hasAnyRole('Admin')")
     public ProductResponse updateStock(@PathVariable Long id, @Valid @RequestBody StockUpdateRequest request) {
         return service.updateStock(id, request);
     }
